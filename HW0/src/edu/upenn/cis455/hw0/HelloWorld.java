f131624ab68731c804417fd386510ab86889e4c9
Ao Sun <sunao1@seas.upenn.edu>
2017-01-21 16:03:50 -0500

    Solved HW0

--- HW0/src/edu/upenn/cis455/hw0/HelloWorld.java
+++ HW0/src/edu/upenn/cis455/hw0/HelloWorld.java
@@ -1,5 +1,9 @@
 package edu.upenn.cis455.hw0;
 
 public class HelloWorld {
-  // TODO: Add your code here
+	
+  public static void main(String[] args) {
+	  String output = "*** Author: Ao Sun (sunao1)";
+	  System.out.println(output);
+  }
 }
